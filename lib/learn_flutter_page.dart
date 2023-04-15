0e0f1ede9a4ddb5eb97eb7f480b89fd578618156
Rohan Sawai <sawairohan90@gmail.com>
2023-04-15 11:50:40 +0530

    first commit

---
+++ lib/learn_flutter_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({super.key});
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPage();
+}
+
+class _LearnFlutterPage extends State<LearnFlutterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Learn Flutter'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: Column(
+        children: [
+          Image.asset('images/einstein.jpg'),
+          const SizedBox(
+            height: 20,
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
+            color: Colors.amber,
+            width: double.infinity,
+            child: const Center(
+              child: Text(
+                'This is a text widget',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/profile_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_testing/learn_flutter_page.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text('Item ${(index + 1)}'),
+          leading: const Icon(Icons.person),
+          trailing: const Icon(Icons.select_all),
+          onTap: () {
+            debugPrint('Item ${(index + 1)} selected');
+          },
+        );
+      },
+    );
+  }
+}
